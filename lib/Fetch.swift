dd965c8150c49eb25648771b2e4223b594017a0d
Sergii Kutnii <mnkutster@gmail.com>
2018-02-07 21:24:11 +0200

    Implementation changed

--- lib/Fetch.swift
+++ lib/Fetch.swift
@@ -8,65 +8,72 @@
 
 import Foundation
 
+public enum FetchError : Error {
+    case connectionError
+    case httpError(Int)
+    case noData
+    case invalidLink(String)
+    case parseError
+}
+
 class Fetch {
     
-    class func request(_ request: URLRequest) -> Promise {
+    class func request(_ request: URLRequest) -> Promise<Data> {
         return Promise {
             resolve, reject in
             
             URLSession.shared.dataTask(with: request) {
                 data, response, error in
                 guard nil == error else {
-                    reject("Connection error")
+                    reject(FetchError.connectionError)
                     return
                 }
                 
                 let code = (response as? HTTPURLResponse)?.statusCode ?? 400
                 guard code <= 400 else {
-                    reject("HTTP error \(code)")
+                    reject(FetchError.httpError(code))
                     return
                 }
                 
                 guard nil != data else {
-                    reject("No data")
+                    reject(FetchError.noData)
                     return
                 }
                 
-                resolve(data)
+                resolve(data!)
             } .resume()
         }
     }
     
-    class func url(_ url: URL) -> Promise {
+    class func url(_ url: URL) -> Promise<Data> {
         return request(URLRequest(url:url))
     }
     
-    class func from(_ link: String) -> Promise {
+    class func from(_ link: String) -> Promise<Data> {
         let url = URL(string: link)
         guard nil != url else {
-            return Promise.reject("Invalid URL \(link)")
+            return Promise.reject(FetchError.invalidLink(link))
         }
         
         return request(URLRequest(url: url!))
     }
     
-    class func json(request: URLRequest) -> Promise {
-        return self.request(request).then  {
-            result in
-            let data = result as! Data
+    class func json(request: URLRequest) -> Promise<Any> {
+        return self.request(request).then(async:)  {
+            data in
             do {
                 let content = try JSONSerialization.jsonObject(with: data, options: [])
-                return content
+                return Promise<Any>.resolve(content)
             } catch {
-                return Promise.reject("Parse error")
+                return Promise<Any>.reject(FetchError.parseError)
             }
         }
     }
     
-    class func json(_ link: String) -> Promise {
+    class func json(_ link: String) -> Promise<Any> {
         let url = URL(string: link)
         guard nil != url else {
-            return Promise.reject("Invalid URL \(link)")
+            return Promise<Any>.reject(FetchError.invalidLink(link))
         }
         
         return json(request: URLRequest(url: url!))
